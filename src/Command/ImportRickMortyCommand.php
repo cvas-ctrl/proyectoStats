2d3f66d2705ed38e1fbc67ae060403fe5142aeda
cvas-ctrl <saramayoayarde@safareyes.es>
2026-01-19 20:56:33 +0100

    Controller del personaje

--- src/Command/ImportRickMortyCommand.php
+++ src/Command/ImportRickMortyCommand.php
@@ -8,15 +8,13 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:import-rick-morty',
-    description: 'Importamos los personajes',
+    description: 'Importa personajes y localizaciones evitando duplicados',
 )]
 class ImportRickMortyCommand extends Command
 {
@@ -30,61 +28,79 @@ class ImportRickMortyCommand extends Command
         parent::__construct();
     }
 
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        try {
-            $response = $this->httpClient->request('GET', 'https://rickandmortyapi.com/api/character', [
-                'verify_peer' => false,
+        $responseChar = $this->httpClient->request('GET', 'https://rickandmortyapi.com/api/character', ['verify_peer' => false]);
+        $dataChar = $responseChar->toArray();
+
+        $catPersonajes = $this->entityManager->getRepository(Categoria::class)->findOneBy(['nombre' => 'Personajes']);
+        if (!$catPersonajes) {
+            $catPersonajes = new Categoria();
+            $catPersonajes->setNombre('Personajes');
+            $this->entityManager->persist($catPersonajes);
+        }
+
+        foreach ($dataChar['results'] as $charData) {
+            $elemento = $this->entityManager->getRepository(Elemento::class)->findOneBy([
+                'idApi' => $charData['id'],
+                'categoria' => $catPersonajes
             ]);
 
-            $data = $response->toArray();
-
-            $categoria = $this->entityManager->getRepository(Categoria::class)->findOneBy(['nombre' => 'Personajes']);
-
-            if (!$categoria) {
-                $categoria = new Categoria();
-                $categoria->setNombre('Personajes');
-                $this->entityManager->persist($categoria);
-            }
-
-            foreach ($data['results'] as $charData) {
+            if (!$elemento) {
                 $elemento = new Elemento();
-                $elemento->setNombre($charData['name']);
-                $elemento->setCategoria($categoria);
                 $elemento->setIdApi($charData['id']);
-
-                $elemento->setImagenUrl($charData['image']);
-
-                $elemento->setDatosExtra([
-                    'status' => $charData['status'],
-                    'species' => $charData['species'],
-                    'gender' => $charData['gender']
-                ]);
-
+                $elemento->setCategoria($catPersonajes);
                 $elemento->setPuntuacionPromedio(0);
                 $elemento->setTotalValoraciones(0);
-
                 $this->entityManager->persist($elemento);
             }
 
-            $this->entityManager->flush();
-            $io->success('Importado');
-
-        } catch (\Exception $e) {
-            $io->error('Error de conexión: ' . $e->getMessage());
-            return Command::FAILURE;
+            $elemento->setNombre($charData['name']);
+            $elemento->setImagenUrl($charData['image']);
+            $elemento->setDatosExtra([
+                'status' => $charData['status'],
+                'species' => $charData['species']
+            ]);
         }
 
+        $responseLoc = $this->httpClient->request('GET', 'https://rickandmortyapi.com/api/location', ['verify_peer' => false]);
+        $dataLoc = $responseLoc->toArray();
+
+        $catLoc = $this->entityManager->getRepository(Categoria::class)->findOneBy(['nombre' => 'Localizaciones']);
+        if (!$catLoc) {
+            $catLoc = new Categoria();
+            $catLoc->setNombre('Localizaciones');
+            $this->entityManager->persist($catLoc);
+        }
+
+        foreach ($dataLoc['results'] as $locData) {
+            $elemento = $this->entityManager->getRepository(Elemento::class)->findOneBy([
+                'idApi' => $locData['id'],
+                'categoria' => $catLoc
+            ]);
+
+            if (!$elemento) {
+                $elemento = new Elemento();
+                $elemento->setIdApi($locData['id']);
+                $elemento->setCategoria($catLoc);
+                $elemento->setPuntuacionPromedio(0);
+                $elemento->setTotalValoraciones(0);
+                $this->entityManager->persist($elemento);
+            }
+
+            $elemento->setNombre($locData['name']);
+            $elemento->setImagenUrl('https://via.placeholder.com/300?text=Location');
+            $elemento->setDatosExtra([
+                'type' => $locData['type'],
+                'dimension' => $locData['dimension']
+            ]);
+        }
+
+        $this->entityManager->flush();
+
+        $io->success('Importado');
         return Command::SUCCESS;
     }
 }
