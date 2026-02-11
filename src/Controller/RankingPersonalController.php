c2c8d65c618b87d1b6021d202c1a88c67a9605e3
cvas-ctrl <saramayoayarde@safareyes.es>
2026-02-11 10:26:14 +0100

    Intento de editar ranking

--- src/Controller/RankingPersonalController.php
+++ src/Controller/RankingPersonalController.php
@@ -51,20 +51,31 @@ final class RankingPersonalController extends AbstractController
     }
 
     #[Route('/procesar-mi-ranking', name: 'app_ranking_guardar', methods: ['POST'])]
-    public function guardar(Request $request, EntityManagerInterface $em, ElementoRepository $elRepo): Response
+    public function guardar(Request $request, EntityManagerInterface $em, ElementoRepository $elRepo, RankingRepository $rankingRepo): Response
     {
         $nombre = $request->request->get('nombre_ranking');
+        $usuario = $this->getUser();
         $params = $request->request->all();
         $idsOrdenados = $params['elementos_seleccionados'] ?? [];
 
         if (!$nombre || empty($idsOrdenados)) {
+            $this->addFlash('warning', 'El ranking debe tener un nombre y al menos un elemento');
+            return $this->redirectToRoute('app_ranking_nuevo');
+        }
+
+        $existe = $rankingRepo->findOneBy([
+            'usuario' => $usuario,
+            'nombreRanking' => $nombre
+        ]);
+
+        if ($existe) {
+            $this->addFlash('danger', 'Ya has creado un ranking con el nombre "' . $nombre . '". Prueba con otro o edita el anterior');
             return $this->redirectToRoute('app_ranking_nuevo');
         }
 
         $ranking = new Ranking();
         $ranking->setNombreRanking($nombre);
-        $ranking->setUsuario($this->getUser());
-
+        $ranking->setUsuario($usuario);
 
         $em->persist($ranking);
 
@@ -81,7 +92,51 @@ final class RankingPersonalController extends AbstractController
 
         $em->flush();
 
+        $this->addFlash('success', 'Ranking personalizado');
         return $this->redirectToRoute('app_ranking_ver_final', ['id' => $ranking->getId()]);
+    }
+
+    #[Route('/ranking/editar/{id}', name: 'app_ranking_editar')]
+    public function editar(Ranking $ranking, Request $request, EntityManagerInterface $em, ElementoRepository $elRepo, CategoriaRepository $catRepo): Response
+    {
+        if ($ranking->getUsuario() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('No puedes editar este ranking');
+        }
+
+        if ($request->isMethod('POST')) {
+            $nuevoNombre = $request->request->get('nombre_ranking');
+            $idsOrdenados = $request->request->all()['elementos_seleccionados'] ?? [];
+
+            if ($nuevoNombre && !empty($idsOrdenados)) {
+                $ranking->setNombreRanking($nuevoNombre);
+
+                foreach ($ranking->getRankingElementos() as $antiguo) {
+                    $em->remove($antiguo);
+                }
+                $em->flush();
+
+                foreach ($idsOrdenados as $indice => $id) {
+                    $elemento = $elRepo->find($id);
+                    if ($elemento) {
+                        $re = new RankingElemento();
+                        $re->setRanking($ranking);
+                        $re->setElemento($elemento);
+                        $re->setPosicion($indice + 1);
+                        $em->persist($re);
+                    }
+                }
+
+                $em->flush();
+                $this->addFlash('success', 'Ranking actualizado');
+                return $this->redirectToRoute('app_mis_rankings_lista');
+            }
+        }
+
+        return $this->render('ranking_personal/editar.html.twig', [
+            'ranking' => $ranking,
+            'elementos' => $elRepo->findAll(),
+            'categorias' => $catRepo->findAll()
+        ]);
     }
 
     #[Route('/ranking/ver/{id}', name: 'app_ranking_ver_final')]
